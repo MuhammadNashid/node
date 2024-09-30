21b05370eee1c593b1ddb89c30956b40b4b1637b
MuhammadNashid <muhammadnashid905@gmail.com>
2024-09-30 11:43:19 +0530

    file added

--- serverside/server.js
+++ serverside/server.js
@@ -18,60 +18,67 @@ console.log(path);
 
 if(path.pathname=="/"){
     res.writeHead(200,{"Content-Type":"text/html"});
-    res.end(fs.readFileSync("../clientside/index.html"));
+    res.end(fs.readFileSync("../clientside/index.html"))
 }
-else if(path.pathname=='/clientside/css/index.css'){
+else if(path.pathname=="./clientside/css/style.css"){
     res.writeHead(200,{"Content-Type":"text/css"});
-    res.end(fs.readFileSync("../clientside/css/index.css"));
+    res.end(fs.readFileSync("../clientside/css/style.css"))
 }
+
 else if(path.pathname=='/js/custom.js'){
     res.writeHead(200,{"Content-Type":"text/css"});
     res.end(fs.readFileSync('../clientside/js/custom.js'));
 }
-else if(path.pathname=='/pages/AddDonor'){
+else if(path.pathname=='/pages/add'){
     res.writeHead(200,{"Content-Type":"text/html"});
-    res.end(fs.readFileSync('../clientside/pages/AddDonor.html'));
+    res.end(fs.readFileSync('../clientside/pages/add.html'));
 }
-else if(path.pathname=='/CSS/style.css'){
+
+else if(path.pathname=='/css/style.css'){
     res.writeHead(200,{"Content-Type":"text/css"});
-    res.end(fs.readFileSync('../clientside/pages/CSS/style.css'));
+    res.end(fs.readFileSync('../clientside/css/style.css'));
 }
-if(path.pathname=='/sumbit'&&req.method=='POST'){
-    console.log("hai");
-    let body="";
-    req.on("data",(chunks)=>{
-        console.log(chunks);
-        body+=chunks.toString();
-        console.log(body);
-            
-    });
-req.on("end",async()=>{
-    if(body!==null){
-        const formData=queryString.parse(body);
-        console.log(formData);
-        collection.insertOne(formData).then(()=>{
-            console.log("data added");
-            
-        }).catch((error)=>{
-            console.log(error);
-            
-        })
+// if(path.pathname=='/sumbit'&&req.method=='POST'){
+//     console.log("hai");
+//     let body="";
+//     req.on("data",(chunks)=>{
+//         console.log(chunks);
 
-        res.writeHead(200,{"Content-Type":"text/html"})
-        res.end(fs.readFileSync("../clientside/index.html"))
-    }
-})
+//         body+=chunks.toString();
+//         console.log(body);
+            
+//     });res.writeHead(200,{"Content-Type":"text/html"});
+//     res.end(fs.readFileSync("../clientside/index.html"))
+// req.on("end",async()=>{
+//     if(body!==null){
+//         const formData=queryString.parse(body);
+//         console.log(formData);
+//         collection.insertOne(formData).then(()=>{
+//             console.log("data added");
+            
+//         }).catch((error)=>{
+//             console.log(error);
+            
+//         })
+
+//         res.writeHead(200,{"Content-Type":"text/html"})
+//         res.end(fs.readFileSync("../clientside/index.html"))
+//     }
+// })
     
-}
-// get donors
+// }
 
-        if(path.pathname=="/getdonors"&&req.method=="GET"){
-            const data=await collection.find().toArray();
-            const json_data=JSON.stringify(data)
-            console.log(json_data);
+// // get donors
+
+//         if(path.pathname=="/getdonors"&&req.method=="GET"){
+//             const data=await collection.find().toArray();
+//             const json_data=JSON.stringify(data)
+//             console.log(json_data);
+
+
             
 
-        }
+//         }
 
 })
 app.listen(PORT);

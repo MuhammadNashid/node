586dd23d53c90c5176dc630d779b15691d741f0f
MuhammadNashid <muhammadnashid905@gmail.com>
2024-10-01 11:36:02 +0530

    file added

--- serverside/server.js
+++ serverside/server.js
@@ -1,12 +1,10 @@
 const http=require("http")
 const fs=require("fs")
 const url=require("url")
-// const queryString=require("queryString")
 const queryString=require("querystring")
 const {MongoClient, ObjectId}=require("mongodb");
 const { error } = require("console");
-// const { error } = require("console");
-const client= new MongoClient("mongodb://127.0.0.1:27017/");
+const client= new MongoClient('mongodb://127.0.0.1:27017/');
 const PORT=3000;
 
 const app=http.createServer(async(req,res)=>{
@@ -20,13 +18,13 @@ console.log(path);
 
 if(path.pathname=="/"){
     res.writeHead(200,{"Content-Type":"text/html"});
-    res.end(fs.readFileSync("../clientside/index.html"))
+    res.end(fs.readFileSync("../clientside/pages/index.html"))
 }
 else if(path.pathname=='/css/style.css'){
     res.writeHead(200,{"Content-Type":"text/css"});
     res.end(fs.readFileSync("../clientside/css/style.css"))
 }
-else if(path.pathname=='/pages/add'){
+else if(path.pathname=='/add'){
     res.writeHead(200,{"Content-Type":"text/html"});
     res.end(fs.readFileSync("../clientside/pages/add.html"))
 }
@@ -38,26 +36,26 @@ else if(path.pathname=='/js/custom.js'){
     res.writeHead(200,{"Content-Type":"text/js"});
     res.end(fs.readFileSync('../clientside/js/custom.js'));
 }
-else if(path.pathname=='/js/add.js'){
+else if(path.pathname=='/add'){
     res.writeHead(200,{"Content-Type":"text/js"});
     res.end(fs.readFileSync('../clientside/js/add.js'));
 }
-if(path.pathname=="/sumbit"&&req.method=="POST"){
+if(path.pathname=='/sumbit' && req.method=="POST"){
     console.log("hai");
-    let body="";
+    let body='';
     req.on("data",(chunks)=>{
         console.log(chunks);
 
         body+=chunks.toString();
         console.log(body);
-            
+        
     });
 req.on("end",async()=>{
-    if(body!==null){
+    if(body!=null){
         const formData=queryString.parse(body);
         console.log(formData);
         collection.insertOne(formData).then(()=>{
-
+console.log("data added");
         }).catch((error)=>{
             console.log(error);
         })
@@ -70,14 +68,14 @@ req.on("end",async()=>{
      }
      //getDonors
 
-    else if(path.pathname=="/getdonors"&&req.method=="GET"){
+    else if(path.pathname=='/getdonor' && req.method=='GET'){
         const data=await collection.find().toArray();
         const json_data=JSON.stringify(data)
         console.log(json_data);
         res.writeHead(200,{"Content-Type":"text/json"})
         res.end(json_data)
      }
-    else if(path.pathname=='/delete'&& req.method=='DELETE'){
+    else if(path.pathname=='/delete' && req.method=='DELETE'){
         console.log("=========================DELETE==========================");
         
         let body=''
